ec07ed4209c23df1917d7853c857d8b5346750db
Ben <benjamin@thomasnetwork.net>
2013-01-30 19:34:34 -0500

    Updated load file GUI + new file function in fileManager

--- saved/fileManager.php
+++ saved/fileManager.php
@@ -4,21 +4,42 @@ if($_GET['task'])
 	$task = $_GET['task'];
 else
 	$task = ' ';
-
+if($_GET['file'])
+	$file = $_GET['file'];
+else
+	$file = null;
 
 switch($task){
+	case 'makeNewFile':
+	if($file == null)
+		echo 'ERROR: could not save file, name not defined.';
+	$newFile = new DOMDocument();
+	$root = $newFile->createElement('content');
+	$blank = $newFile->createTextNode('');
+	
+		
+	$newFile->appendChild($root);
+	$root->appendChild($blank);
+	
+	$newFile->save($file.'.xml');
+	
+	case 'saveFile':
+		if($task == 'saveFile'){
+			
+		}
+		
 	case 'getFiles':
 		$fileNames = scandir('.');
 		foreach($fileNames as $fileName){
 			$ext = pathinfo($fileName, PATHINFO_EXTENSION);
 			if($ext == 'xml' || $ext == 'XML')
-				echo 'file: <a href="#" onclick="loadPitFile(\'saved/'.$fileName.'\')">'.$fileName.'</a><br> ';
+				echo 'file: <a href="#" onclick="return false" ondblclick="loadPitFile(\'saved/'.$fileName.'\'); return false">'.$fileName.'</a><br> ';
 		}
 		echo "<br>";
 		break;
 	
-	case 'makeNewFile':
-		echo 'making file';
+	
+		
 		break;
 	default:
 		echo "ERROR: task not defined '".$task."'" ;
